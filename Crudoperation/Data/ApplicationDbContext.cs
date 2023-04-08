8d8a181461fd9f5ed0a04ea47d37b8035f7a4238
monikauprety <monikauprety0011@gmail.com>
2023-04-08 12:37:18 +0545

    Add project files.

---
+++ Crudoperation/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Crudoperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crudoperation.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base (options) 
+		
+		{ }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Crudoperation/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Crudoperation.Models
+{
+	public class Product
+	{
+        public int Id { get; set; }
+		public string? Name { get; set; }
+		public string ?Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public  int Rate { get; set; }
+    }
+}
